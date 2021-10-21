89ac237e46a9fb628a9b7a413fbffb448e51b363
Yesus0212 <jesussolispadron@gmail.com>
2021-10-20 22:17:51 -0500

    Ejercicio Grupal DOM Sesion 14

---
+++ session14/ejercicioGrupalDOM/js/script.js
@@ -0,0 +1,138 @@
+// Crear tabla con el sigueinte arreglo
+
+const mentorsArray = [
+    {
+        name:"Ivan Diaz",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:8
+            },
+            {
+                signature:"ReactJS",
+                score:8
+            }
+        ]
+    },
+    {
+        name:"Alan Medina",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:7
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+    {
+        name:"Elvira Camarillo",
+        scores:[
+            {
+                signature:"HTML",
+                score:9
+            },
+            {
+                signature:"CSS",
+                score:9
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:9
+            }
+        ]
+    },
+    {
+        name:"Alejandra Paez",
+        scores:[
+            {
+                signature:"HTML",
+                score:10
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            },
+        ]
+    },
+  ]
+  
+
+const header = document.getElementById("header_table");
+const body = document.getElementById("body_table");
+
+
+const tr = document.createElement('tr');
+const th = document.createElement('th');
+th.setAttribute("scope", "col"); 
+const text = document.createTextNode("Mentor");
+th.appendChild(text);
+tr.appendChild(th);
+
+mentorsArray[0].scores.forEach((mentors) => { 
+    const th = document.createElement('th');
+    th.setAttribute("scope", "col");   
+    const text = document.createTextNode(mentors.signature);    
+    th.appendChild(text);
+    tr.appendChild(th);
+});
+
+const thP = document.createElement('th');
+thP.setAttribute("scope", "col"); 
+const textP = document.createTextNode("Promedio");
+thP.appendChild(textP);
+tr.appendChild(thP);
+
+header.appendChild(tr);
+
+const trB = document.createElement('tr');
+mentorsArray.forEach((mentor) => {   
+    const tr = document.createElement('tr'); 
+    const th = document.createElement('th');
+    th.setAttribute("scope", "row");   
+    const textName = document.createTextNode(mentor.name);    
+    th.appendChild(textName);
+    tr.appendChild(th);
+
+    mentor.scores.forEach((score) => {
+        const td = document.createElement('td');
+        const textScore = document.createTextNode(score.score); 
+        td.appendChild(textScore);
+        tr.appendChild(td);
+    });    
+
+    body.appendChild(tr);
+});
+
+
+
